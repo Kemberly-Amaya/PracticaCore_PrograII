5e790b00b3ad5f839069a295c31539070c37f75b
Kemberly-Amaya <as0021032020@unab.edu.sv>
2021-09-24 11:38:08 -0600

    Practica Presencial Grupo Martes

---
+++ AppCorePracticaUno/Controllers/PersonaController.cs
@@ -0,0 +1,41 @@
+﻿using AppCorePracticaUno.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCorePracticaUno.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Persona persona)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", persona);
+            }
+            else
+            {
+                return View("Registro");
+                
+            }
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppCorePracticaUno/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCorePracticaUno.Models
+{
+    public class Persona
+    {
+        [Display(Name = "Nombre Usuario")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String NombreUsuario { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String Sexo { get; set; }
+
+        [Display(Name = "Tipo de educacion")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String Tipodeeducacion { get; set; }
+
+    }
+}
